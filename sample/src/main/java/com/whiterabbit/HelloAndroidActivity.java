a7131882395499d8719698325fa4d332021b6dbc
fedepaol <fedepaol@gmail.com>
2012-12-02 22:23:37 +0100

    Basic sample

--- sample/src/main/java/com/whiterabbit/HelloAndroidActivity.java
+++ sample/src/main/java/com/whiterabbit/HelloAndroidActivity.java
@@ -3,11 +3,17 @@ package com.whiterabbit;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.whiterabbit.jcricket.PopupHint;
 
 public class HelloAndroidActivity extends Activity {
 
     private static String TAG = "jcricket_sample";
 
+    Button mButton;
+    PopupHint mHint;
     /**
      * Called when the activity is first created.
      * @param savedInstanceState If the activity is being re-initialized after 
@@ -19,6 +25,20 @@ public class HelloAndroidActivity extends Activity {
         super.onCreate(savedInstanceState);
 		Log.i(TAG, "onCreate");
         setContentView(R.layout.main);
+
+
+        mButton = (Button) findViewById(R.id.main_button);
+        PopupHint.PopupBuilder b = new PopupHint.PopupBuilder(this);
+        mHint = b.layout(R.layout.baloon).location(PopupHint.PopupLocation.TOP_LEFT, PopupHint.PopupCorner.BOTTOM_RIGHT).build();
+        TextView baloonText = (TextView) mHint.getHintSubview(R.id.baloon_text);
+        baloonText.setText("Baloon text\ncan be set programmatically\nPress me to dismiss");
+
+
+    }
+
+
+    public void onMainButtonPressed(View v){
+        mHint.showHint(v);
     }
 
 }
